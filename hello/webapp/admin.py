697fdc6c5a80f06f37a3c4b234b60e6dff04e3f1
bekbolot <nurbekovbekbolot090501@gmail.com>
2021-03-06 12:03:03 +0600

    1 этап закончен.Модель создана в админке всё прописано и в фикстуру добавлен dump

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'description', 'status', 'created_at']
+    list_filter = ['name']
+    fields = ['name', 'email', 'description', 'status']
+
+
+admin.site.register(Book, BookAdmin)
+# Register your models here.
 
 # Register your models here.
--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+STATUS_CHOICES = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="имя автора ")
+    email = models.EmailField(null=False, blank=False, verbose_name="почта автора")
+    description = models.TextField(max_length=1000, null=False, blank=False, verbose_name="текст записи")
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, null=False, blank=False, default="active",
+                              verbose_name="Статус задачи")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    class Meta:
+        db_table = 'books'
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книгии'
+
+    def __str__(self):
+        return f'{self.id}. {self.name}:{self.email} {self.description} {self.status}{self.created_at}{self.updated_at}'
 # Create your models here.
